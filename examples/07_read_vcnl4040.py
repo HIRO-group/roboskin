a9ebe0f488281cbb6532126f69c02b765699be81
Kandai Watanabe <kandai.wata@gmail.com>
2020-01-09 15:40:42 -0700

    added proximity sensor

---
+++ examples/07_read_vcnl4040.py
@@ -0,0 +1,13 @@
+import time
+import board
+import busio
+import adafruit_vcnl4040
+
+i2c = busio.I2C(board.SCL, board.SDA)
+sensor = adafruit_vcnl4040.VCNL4040(i2c)
+
+while True:
+    print('Proximity: ', sensor.proximity)
+    print('Light: ', sensor.lux)
+    time.sleep(0.5)
+
