58d075c6d57fed01f8251704e0c100b464883fba
Kodur Krishna Chaitanya <chaitanyaradon89@gmail.com>
2020-01-29 15:04:23 -0700

    Added Test Connections

---
+++ tests/sensor/test_connections.py
@@ -0,0 +1,13 @@
+from robotic_skin.sensor.lsm6ds3_accel import LSM6DS3_acclerometer
+from robotic_skin.sensor.vl53l1x import VL53L1X_ProximitySensor
+import unittest
+
+
+class TestCircuit(unittest):
+    def test_circuit_connections(self):
+        LSM6DS3_acclerometer()
+        VL53L1X_ProximitySensor()
+
+
+if __name__ == "__main__":
+    unittest.main()
