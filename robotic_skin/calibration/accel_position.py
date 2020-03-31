fixed the optimization code so that it performs 1cm precision

robotic_skin/calibration/accel_position.py
@@ -455,7 +455,7 @@ class KinematicEstimator():
         dofd_r_su = dof_T_su.position
         # Every joint rotates along its own z axis
         w_dofd = np.array([0, 0, curr_w])
-        a_dofd = np.dot(w_dofd, np.dot(w_dofd, dofd_r_su))
+        a_dofd = np.cross(w_dofd, np.cross(w_dofd, dofd_r_su))
 
         g_rs = np.array([0, 0, 9.81])
 

robotic_skin/calibration/plot_data.py
@@ -2,9 +2,10 @@ import os
 import rospkg
 import pickle
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
 
-from robotic_skin.calibration.utils import TransMat, ParameterManager
+from robotic_skin.calibration.utils import TransMat, ParameterManager, n2s
 
 PACKAGE_HOME_DIR = os.path.abspath(__file__ + "/../../../")
 
@@ -67,31 +68,45 @@ def estimate_acceleration_analytically(Tdofs, Tjoints, Tdofi2su, d, i, curr_w):
     dofd_r_su = dof_T_su.position
     # Every joint rotates along its own z axis
     w_dofd = np.array([0, 0, curr_w])
-    a_dofd = np.dot(w_dofd, np.dot(w_dofd, dofd_r_su))
+    a_dofd = np.cross(w_dofd, np.cross(w_dofd, dofd_r_su))
+    # print('[From Joint{} to IMU{}, w={}] r = {}    a = {}'.format(d, i, n2s(w_dofd), n2s(dofd_r_su,3), n2s(a_dofd,3)))
 
-    g_rs = np.array([0, 0, 9.81])
+    g_rs = np.array([0, 0, 9.8])
 
     a_su = np.dot(dof_T_su.R.T, a_dofd) + np.dot(rs_T_su.R.T, g_rs)
+    # a_su = np.dot(rs_T_su.R.T, g_rs)
 
     return a_su
 
 
-def get_su_transmat(i):
-    params = np.array([
-        [1.57,  -0.157,  -1.57, 0.07, 0,  1.57],
-        [-1.57, -0.0925, 1.57,  0.07, 0,  1.57],
-        [-1.57, -0.16,   1.57,  0.05, 0,  1.57],
-        [-1.57, 0.0165,  1.57,  0.05, 0,  1.57],
-        [-1.57, -0.17,   1.57,  0.05, 0,  1.57],
-        [-1.57, 0.0053,  1.57,  0.04, 0,  1.57],
-        [0.0,   0.12375, 0.0,   0.03, 0, -1.57]
-    ])
+def get_su_transmat(i, robot='panda'):
+    if robot == 'saywer':
+        params = np.array([
+            [1.57,  -0.157,  -1.57, 0.07, 0,  1.57],
+            [-1.57, -0.0925, 1.57,  0.07, 0,  1.57],
+            [-1.57, -0.16,   1.57,  0.05, 0,  1.57],
+            [-1.57, 0.0165,  1.57,  0.05, 0,  1.57],
+            [-1.57, -0.17,   1.57,  0.05, 0,  1.57],
+            [-1.57, 0.0053,  1.57,  0.04, 0,  1.57],
+            [0.0,   0.12375, 0.0,   0.03, 0, -1.57]
+        ])
+    elif robot == 'panda':
+        params = np.array([
+            [1.57, -0.15, -1.57, 0.05, 0, 1.57],
+            [1.57, 0.06, -1.57, 0.06, 0, 1.57],
+            [0, -0.08, 0, 0.05, 0, 1.57],
+            [-1.57, 0.08, 1.57, 0.06, 0, 1.57],
+            [3.14, -0.1, 3.14, 0.1, 0, 1.57],
+            [-1.57, 0.03, 1.57, 0.05, 0, 1.57],
+            [1.57, 0, -1.57, 0.05, 0, 1.57]
+        ])
+    else:
+        raise NotImplementedError("Define a robot's DH Parameters")
 
     Tdof2vdof = TransMat(params[i, :2])
     Tvdof2su = TransMat(params[i, 2:])
 
     return Tdof2vdof.dot(Tvdof2su)
-
 
 if __name__ == '__main__':
     Data = load_data()
@@ -150,27 +165,45 @@ if __name__ == '__main__':
                     A_measured = np.linalg.norm(meas_accels, axis=1)
                     A_model = np.linalg.norm(model_accels, axis=1)
 
-                    fig = plt.figure(figsize=(12, 8))
-                    ax = fig.add_subplot(311)
+                    fig = plt.figure(figsize=(16, 12))
+                    gs = fig.add_gridspec(3, 2)
+
+                    ax = fig.add_subplot(gs[0, 0])
                     ax.plot(np.arange(meas_accels.shape[0]), A_measured, label='Measured')
-                    ax.plot(np.arange(model_accels.shape[0]), A_model, label='Model')
+                    ax.plot(np.arange(model_accels.shape[0]), A_model, label='Estimated')
                     ax.hlines(y=9.81, xmin=0, xmax=meas_accels.shape[0], color='r', label='G=9.81')
+                    ax.set_title('Acceleration Norm')
                     ax.set_ylim([9, 11])
                     ax.legend()
 
-                    ax = fig.add_subplot(312)
-                    labels = ['x', 'y', 'z']
-                    for j, label in enumerate(labels):
-                        ax.plot(np.arange(meas_accels.shape[0]), meas_accels[:, j], label='Measured ' + label)
-                    ax.set_ylim([-11, 11])
+                    ax = fig.add_subplot(gs[0, 1])
+                    ax.plot(np.arange(meas_accels.shape[0]), meas_accels[:, 0], label='Measured')
+                    ax.plot(np.arange(model_accels.shape[0]), model_accels[:, 0], label='Estimated')
+                    ax.set_title('Acceleration for x axis')
+                    # ax.set_ylim([-11, 11])
                     ax.legend()
 
-                    ax = fig.add_subplot(313)
-                    labels = ['x', 'y', 'z']
-                    for j, label in enumerate(labels):
-                        ax.plot(np.arange(model_accels.shape[0]), model_accels[:, j], label='Model ' + label)
-                    ax.set_ylim([-11, 11])
+                    ax = fig.add_subplot(gs[1, 0])
+                    ax.plot(np.arange(meas_accels.shape[0]), meas_accels[:, 1], label='Measured')
+                    ax.plot(np.arange(model_accels.shape[0]), model_accels[:, 1], label='Estimated')
+                    ax.set_title('Acceleration for y axis')
+                    # ax.set_ylim([-11, 11])
                     ax.legend()
+
+                    ax = fig.add_subplot(gs[1, 1])
+                    ax.plot(np.arange(meas_accels.shape[0]), meas_accels[:, 2], label='Measured')
+                    ax.plot(np.arange(model_accels.shape[0]), model_accels[:, 2], label='Estimated')
+                    ax.set_title('Acceleration for z axis')
+                    # ax.set_ylim([-11, 11])
+                    ax.legend()
+
+                    ax = fig.add_subplot(gs[2, :])
+                    ax.plot(np.arange(angular_velocities.size), angular_velocities)
+                    ax.set_title('Joint Angular Velocity [rad/s]')
+                    if np.sum(angular_velocities) >= 0:
+                        ax.set_ylim([0, 1.1])
+                    else:
+                        ax.set_ylim([-1.1, 0])
 
                     savepath = os.path.join(images_dir, 'max_accel_Pose%i_Joint%i_IMU%i.png' % (p, d, i))
                     print(savepath)