10bd24e127ea1f45032e8492623d4112a4bb68cb
Matthew Strong <mast4878@colorado.edu>
2020-04-02 17:15:54 -0600

    first batch of function docstrings

--- robotic_skin/calibration/loss.py
+++ robotic_skin/calibration/loss.py
@@ -2,6 +2,9 @@ import numpy as np
 
 
 class Loss():
+    """
+    Loss function base class
+    """
     def __init__(self):
         pass
 
@@ -10,6 +13,9 @@ class Loss():
 
 
 class L1Loss(Loss):
+    """
+    L1 Loss
+    """
     def __init__(self):
         super().__init__()
 
@@ -18,6 +24,9 @@ class L1Loss(Loss):
 
 
 class L2Loss(Loss):
+    """
+    L2Loss.
+    """
     def __init__(self):
         super().__init__()
 
