aef7183e554792d3ef35db127700ae6d175c11a0
Krishna <chaitanyaradon89@gmail.com>
2020-04-22 23:14:00 -0600

    Stats generator initial commit

---
+++ robotic_skin/calibration/stats_generator.py
@@ -0,0 +1,7 @@
+"""
+The stats generator file will output the following stats:
+1) A graph with SU's euclidean distance between real and predicted points.
+    One legend will be Mittendorfer's method and another will be ours
+2) A table comparing the dh params individually of our method and the others
+3) A table comparing the orientation differences between Mittendorfer's method and ours
+"""
