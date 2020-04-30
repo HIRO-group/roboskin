fix quaternion test

tests/calibration/test_utils.py
@@ -419,7 +419,7 @@ class QuaternionTest(unittest.TestCase):
         a = np.array([a[1], a[2], a[3], a[0]])
         b = np.array([b[1], b[2], b[3], b[0]])
         angle_in_degrees = utils.angle_between_quaternions(a, b)
-        self.assertAlmostEqual(angle_in_degrees, 150)
+        self.assertAlmostEqual(angle_in_degrees, 2.6179938779914944)
 
 
 if __name__ == '__main__':