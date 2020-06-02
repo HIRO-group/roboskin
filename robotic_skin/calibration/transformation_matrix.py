45684757329c3076b2c06d69aa4bc3132ab9dc10
Matthew Strong <mast4878@colorado.edu>
2020-06-01 21:40:56 -0600

    fix tests.

--- robotic_skin/calibration/transformation_matrix.py
+++ robotic_skin/calibration/transformation_matrix.py
@@ -46,6 +46,8 @@ class TransformationMatrix():
             params = np.array([theta, d, a, alpha], dtype=float)
             # Only select provided keys (which are not None)
             self.params = np.nan_to_num(params)
+            temp_params = self.params
+
         self.matrix = self.transformation_matrix(*self.params)
         qx = pyqt.Quaternion(axis=[1, 0, 0], angle=temp_params[3])
         qz = pyqt.Quaternion(axis=[0, 0, 1], angle=temp_params[0])
