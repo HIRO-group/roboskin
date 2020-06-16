86cece6927e58dfcffe802cbc2ec5dc373cf6680
Krishna <chaitanyaradon89@gmail.com>
2020-06-15 21:44:15 -0600

    Test

--- robotic_skin/sensor/lsm6ds3.py
+++ robotic_skin/sensor/lsm6ds3.py
@@ -16,6 +16,7 @@ class LSM6DS3_IMU(Sensor):
     This is the Python Class for LSM6DS3. This includes all subroutines including calibration to handle everything
     related to the device.
     """
+
     def __init__(self, config_file):  # noqa: E999
         """
         Initializes the LSM6DS3 accelerometer. Checks for the I2C connection and checks whether it's the correct
@@ -206,13 +207,13 @@ class LSM6DS3_IMU(Sensor):
     def _read_raw(self):
         axh = self.read_reg(self.OUTX_H_XL)
         axl = self.read_reg(self.OUTX_L_XL)
-        ax = self.make_16bit_value(axh, axl) * 0.000488 * GRAVITATIONAL_CONSTANT
+        ax = self.make_16bit_value(axh, axl) * GRAVITATIONAL_CONSTANT
         ayh = self.read_reg(self.OUTY_H_XL)
         ayl = self.read_reg(self.OUTY_L_XL)
-        ay = self.make_16bit_value(ayh, ayl) * 0.000488 * GRAVITATIONAL_CONSTANT
+        ay = self.make_16bit_value(ayh, ayl) * GRAVITATIONAL_CONSTANT
         azh = self.read_reg(self.OUTZ_H_XL)
         azl = self.read_reg(self.OUTZ_L_XL)
-        az = self.make_16bit_value(azh, azl) * 0.000488 * GRAVITATIONAL_CONSTANT
+        az = self.make_16bit_value(azh, azl) * GRAVITATIONAL_CONSTANT
         gxh = self.read_reg(self.OUTX_H_G)
         gxl = self.read_reg(self.OUTX_L_G)
         gx = self.make_16bit_value(gxh, gxl)
@@ -255,9 +256,9 @@ class LSM6DS3_IMU(Sensor):
         """
         return [self._calibrate_value(each_value) for each_value in self._read_raw()]
 
+
 if __name__ == "__main__":
     lsm6ds3 = LSM6DS3_IMU("/home/hiro/catkin_ws/src/ros_robotic_skin/config/accelerometer_config1.yaml")
     while 1:
         print(lsm6ds3.read()[0:3])
         sleep(0.5)
-
