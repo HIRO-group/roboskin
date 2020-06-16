21d9f15ab92351c0fe25855fa2555c02ee72e4bb
Krishna <chaitanyaradon89@gmail.com>
2020-06-15 21:06:37 -0600

    Test

--- robotic_skin/sensor/lsm6ds3.py
+++ robotic_skin/sensor/lsm6ds3.py
@@ -256,5 +256,6 @@ class LSM6DS3_IMU(Sensor):
 
 if __name__ == "__main__":
     lsm6ds3 = LSM6DS3_IMU("/home/hiro/catkin_ws/src/ros_robotic_skin/config/accelerometer_config1.yaml")
-    print(lsm6ds3.read())
+    while 1:
+        print(lsm6ds3.read())
 
