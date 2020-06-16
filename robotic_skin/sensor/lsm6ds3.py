59db0257ee9b762200992dfff44c96bdd391a218
Krishna <chaitanyaradon89@gmail.com>
2020-06-15 23:21:03 -0600

    Test

--- robotic_skin/sensor/lsm6ds3.py
+++ robotic_skin/sensor/lsm6ds3.py
@@ -8,6 +8,7 @@ import math
 import smbus2
 from robotic_skin.sensor import Sensor
 from robotic_skin.const import GRAVITATIONAL_CONSTANT
+from time import sleep
 
 
 class LSM6DS3_IMU(Sensor):
@@ -260,3 +261,4 @@ if __name__ == "__main__":
     lsm6ds3 = LSM6DS3_IMU("/home/hiro/catkin_ws/src/ros_robotic_skin/config/accelerometer_config1.yaml")
     while 1:
         print(lsm6ds3.read()[0:3])
+        sleep(0.5)
